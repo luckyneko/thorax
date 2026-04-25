d78dfa469822fb8b98a5f3ba63edf0b23654bb84
Aaron Brown <feik@luckyneko.com>
2026-04-25 12:48:54 +1000

    CMake Install & Package Support

---
+++ test/consumer/main.cpp
@@ -0,0 +1,21 @@
+/*
+ *  Minimal consumer that verifies find_package(Thorax) wires up headers and
+ *  the static library correctly.  Built and run as part of the install smoke-test.
+ */
+
+#include <thx/thorax.h>
+
+#include <cstdio>
+
+int main()
+{
+	std::printf("Thorax %u.%u.%u\n",
+	            thx::THORAX_VERSION.major,
+	            thx::THORAX_VERSION.minor,
+	            thx::THORAX_VERSION.patch);
+
+	thx::ServiceManager sm;
+	auto services = sm.list_services();
+	std::printf("Services registered: %zu\n", services.size());
+	return 0;
+}
